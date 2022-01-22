0f51f8468e8b189c404d6401f17bf864d5d7e3db
Leonidas <jesus.leon1@unmsm.edu.pe>
2022-01-22 04:05:28 -0500

    fix: upgrade vfinal

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>BOLSA DE VALORES</h1>
-      <iframe
-      src="https://app.powerbi.com/view?r=eyJrIjoiNWJmNDYzNDgtMmZhYy00MzFkLTg3YjctYTVjYzI0NDRhOWU2IiwidCI6ImUzOWEwZDFhLWJhOTUtNDMzMi04N2M5LTk5Mjc4YzNiZDE4NyJ9" 
-      title="Bi-web"
-      height="600 " width="1000"
-      ></iframe>
+      <div className="container-title">
+        <h1 className="title">BOLSA DE VALORES</h1>
+      </div>
+      <div className="container-url">
+        <iframe
+        className="url"
+        src="https://app.powerbi.com/view?r=eyJrIjoiNWJmNDYzNDgtMmZhYy00MzFkLTg3YjctYTVjYzI0NDRhOWU2IiwidCI6ImUzOWEwZDFhLWJhOTUtNDMzMi04N2M5LTk5Mjc4YzNiZDE4NyJ9" 
+        title="Bi-web"
+        height="580" width="900"
+        />
+      </div>
     </div>
   );
 }
